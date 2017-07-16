eed68b94aeca0a241e584af4349911af6ba1fb82
kalin.eh <kalin.eh@gmail.com>
2017-07-15 18:54:08 -0700

    training wip

--- think/Assets/MoverNN.cs
+++ think/Assets/MoverNN.cs
@@ -41,6 +41,37 @@ public class NeuralNetwork
         }
     }
 
+    public void CopyWeights(NeuralNetwork other)
+    {
+        for (int i = 0; i < graph.Length; ++i)
+        {
+            var layer = graph[i];
+            for (int j = 0; j < layer.neurons.Length; ++j)
+            {
+                var neuron = layer.neurons[j];
+                for (int k = 0; k < neuron.weights.Length; ++k)
+                    neuron.weights[k] = other.graph[i].neurons[j].weights[k];
+            }
+        }
+    }
+
+    public void MutateWeights(float mutation)
+    {
+        for (int i = 0; i < graph.Length; ++i)
+        {
+            var layer = graph[i];
+            for (int j = 0; j < layer.neurons.Length; ++j)
+            {
+                var neuron = layer.neurons[j];
+                for (int k = 0; k < neuron.weights.Length; ++k)
+                {
+                    if (Random.Range(0.0f, 1.0f) < mutation)
+                        neuron.weights[k] = Random.Range(-0.5f, 0.5f);
+                }
+            }
+        }
+    }
+
     public void Forward(float[] inputs, float[] output)
     {
         var layerFirst = graph[0];
@@ -59,7 +90,7 @@ public class NeuralNetwork
         for (int i = 1; i < graph.Length; ++i)
         {
             var layerCurr = graph[i];
-            var layerPrev = graph[i];
+            var layerPrev = graph[i - 1];
 
             for (int j = 0; j < layerCurr.neurons.Length; ++j)
             {
@@ -74,7 +105,7 @@ public class NeuralNetwork
         }
 
         for (int i = 0; i < output.Length; ++i)
-            output[i] = layerLast.neurons[i].sum;
+            output[i] = 1.0f / (1.0f + Mathf.Exp(layerLast.neurons[i].sum));
 
     } 
 }
@@ -83,6 +114,10 @@ public class MoverNN
     : MonoBehaviour
 {
     private Rigidbody body;
+    private float energy;
+
+    private Vector3 prevPosition;
+    private int stationaryFrames;
 
     // input count N
     // neuron count M
@@ -102,7 +137,7 @@ public class MoverNN
     //  /\ /\ /\
     // o  o  o  o
 
-    private NeuralNetwork nn;
+    public NeuralNetwork nn;
 
     public void OnEnable()
     {
@@ -118,10 +153,16 @@ public class MoverNN
 
         var layerCount = 4;
         var neuronCount = 8;
-        var inputCount = 2;
-        var outputCount = 1;
+        var inputCount = 9;
+        var outputCount = 3;
 
         nn = new NeuralNetwork(layerCount, neuronCount, inputCount, outputCount);
+        energy = 1.0f;
+    }
+
+    public void InitFrom(MoverNN rhs)
+    {
+
     }
 
     public void Reset(Vector3 startPos, Quaternion startRot)
@@ -132,28 +173,65 @@ public class MoverNN
         body.MoveRotation(startRot);
         body.velocity = Vector3.zero;
         body.angularVelocity = Vector3.zero;
+
+        prevPosition = startPos;
+        stationaryFrames = 0;
+        energy = 1.0f;
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
         if (!body)
             Init();
 
+        var currPosition = body.position;
+        var moved = currPosition - prevPosition;
+        if (moved.sqrMagnitude < 0.01f * 0.01f)
+            stationaryFrames++;
+        else
+            stationaryFrames = 0;
+        prevPosition = currPosition;
+
         var rot = body.rotation;
         var rotVel = body.angularVelocity;
+        var mask = LayerMask.GetMask("floor");
         var info = new RaycastHit();
-
-        Physics.Raycast(transform.position, Vector3.down, out info);
-
+        var hit = Physics.Raycast(transform.position, Vector3.down, out info, 2.0f, mask);
         var dist = info.distance;
 
-        var input = new float[] { rotVel.x, dist, };
-        var output = new float[] { 0.0f };
+        if (hit == false)
+            dist = 2.0f;
+
+        var input = new float[] { rot.x, rot.y, rot.z, rot.w, rotVel.x, rotVel.y, rotVel.z, dist, energy };
+        var output = new float[] { 0.0f, 0.0f, 0.0f };
 
         nn.Forward(input, output);
         
-        Debug.DrawLine(transform.position, info.point, Color.red, 3.0f);
+        //Debug.DrawLine(transform.position, info.point, Color.red, 3.0f);
+        //Debug.LogFormat("energy: {0}, out: {1}", energy, output[0] * 1000.0f);
 
-        body.AddForce(output[0], 0.0f, 0.0f, ForceMode.Acceleration);
+        energy += 1.0f * Time.deltaTime;
+
+        var available = energy;
+
+        var spinX = Mathf.Min(energy, output[0]);
+        energy -= spinX;
+
+        var spinY = Mathf.Min(energy, output[1]);
+        energy -= spinY;
+
+        var spinZ = Mathf.Min(energy, output[2]);
+        energy -= spinZ;
+
+        energy = Mathf.Clamp01(energy);
+
+        var dt = Time.fixedDeltaTime;
+
+        body.AddForce(spinX / dt, spinY / dt, spinZ / dt, ForceMode.Acceleration);
+    }
+
+    public bool IsStopped()
+    {
+        return stationaryFrames > 60;
     }
 }
--- think/Assets/MoverNNTrainer.cs
+++ think/Assets/MoverNNTrainer.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class MoverNNTrainer
     : MonoBehaviour
 {
-    public MoverNN instance;
+    public GameObject pfb;
+    
+    private List<MoverNN> instances;
+
+    public bool trainingMode;
+    public bool trainingPause;
 
     public void OnEnable()
     {
@@ -14,20 +19,107 @@ public class MoverNNTrainer
 
     public IEnumerator DoCycles()
     {
-        Debug.LogFormat("MoverNNTrainer: training starting for '{0}'", instance.name);
+        Debug.LogFormat("MoverNNTrainer: training starting...");
 
         var startPos = Vector3.zero;
         var startRot = Quaternion.identity;
 
+        var generation = 0;
+        var count = 64;
+
+        instances = new List<MoverNN>();
+        for (int i = 0; i < count; ++i)
+            instances.Add(GameObject.Instantiate(pfb).GetComponent<MoverNN>());
+
         while (true)
         {
-            instance.Reset(startPos, startRot);
+            foreach (var instance in instances)
+                instance.Reset(startPos, startRot);
 
-            yield return new WaitForSeconds(5.0f);
+            var timer = 0.0f;
+            var dt = Time.fixedDeltaTime;
+            var skip = false;
 
-            var ofs = instance.transform.position - startPos;
-            var lsq = ofs.sqrMagnitude;
-            var len = lsq > 0.001f ? ofs.magnitude : 0.0f;
+            if (trainingMode)
+            {
+                Physics.autoSimulation = false;
+                int previewStep = 100;
+                int previewIndex = 0;
+
+                while (timer < 60.0f)
+                {
+                    if (trainingPause == false)
+                    {
+                        Physics.Simulate(dt);
+                        timer = Tick(timer, dt, out skip);
+                        if (skip)
+                            break;
+                    }
+
+                    previewIndex += 1;
+                    if (previewIndex % previewStep == 0 || trainingPause)
+                        yield return null;
+                }
+                yield return null;
+            }
+            else
+            {
+                Physics.autoSimulation = true;
+                while (timer < 60.0f)
+                {
+                    timer = Tick(timer, Time.fixedDeltaTime, out skip);
+                    if (skip)
+                        break;
+                    yield return new WaitForFixedUpdate();
+                }
+            }
+
+            var best = (MoverNN)null;
+            var bestScore = 0.0f;
+
+            foreach (var instance in instances)
+            {
+                var ofs = instance.transform.position - startPos;
+                var lsq = ofs.sqrMagnitude;
+                var len = lsq > 0.001f ? ofs.magnitude : 0.0f;
+
+                if (len > bestScore)
+                {
+                    best = instance;
+                    bestScore = len;
+                }
+            }
+
+            foreach (var instance in instances)
+                instance.nn.CopyWeights(best.nn);
+            foreach (var instance in instances)
+                instance.nn.MutateWeights(0.1f);
+
+            Debug.LogFormat("MoverNNTrainer: generation {0} score {1} (time: {2})", generation, bestScore, timer);
+
+            generation++;
         }
+    }
+
+    public float Tick(float timer, float dt, out bool earlyOut)
+    {
+        timer += dt;
+
+        earlyOut = false;
+
+        var moving = false;
+        foreach (var instance in instances)
+        {
+            if (instance.IsStopped() == false)
+            {
+                moving = true;
+                break;
+            }
+        }
+
+        if (!moving)
+            earlyOut = true;
+
+        return timer;
     }
 }
